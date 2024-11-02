b42b48d86a97c8dc3c0844abd34ac9ea71991cb6
Andrew Steinkruger <amsteinkruger@gmail.com>
2024-11-01 17:01:06 -0700

    Absolute minimum for handling data.

--- fncap_treemap.R
+++ fncap_treemap.R
@@ -1,1 +1,18 @@
 # Check out Treemap for a quick demo.
+
+# Get packages.
+
+library(terra)
+library(magrittr)
+library(readr)
+
+# Get dasta.
+
+#  Get a raster.
+
+dat_raster = "data/TreeMap2016.tif" %>% rast
+
+#  Get a table.
+
+dat_table = "data/Treemap2016_tree_table.csv" %>% read_csv
+
