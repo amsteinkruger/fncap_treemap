110f1afd9744fd6d6f9939867f3f5fa2cce662f8
Andrew Steinkruger <amsteinkruger@gmail.com>
2024-11-02 21:49:07 -0700

    Resolved a couple data mysteries, ran into more.

--- fncap_treemap.R
+++ fncap_treemap.R
@@ -3,8 +3,8 @@
 # Get packages.
 
 library(terra)
+library(tidyverse)
 library(magrittr)
-library(readr)
 
 # Get dasta.
 
@@ -16,3 +16,12 @@ dat_raster = "data/TreeMap2016.tif" %>% rast
 
 dat_table = "data/Treemap2016_tree_table.csv" %>% read_csv
 
+# Mess around.
+
+dat_raster_categories = cats(dat_raster)[[1]] %>% as_tibble
+
+# Comparing dat_table and dat_raster_categories suggests two things:
+#  Value is raster cell ID is tm_id.
+#  The geotiff metadata associated with TreeMap2016.tif is richer, somehow, than the database in Treemap2016_tree_table.csv.
+#  All of this is for 2016.
+#  Uniqueness of observations is not clear from a close look at the data and requires a pass through documentation.
